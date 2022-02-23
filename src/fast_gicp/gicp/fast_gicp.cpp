4c95f969a774e029809942e28c069996122234e8
Tom Noble <tom@rivelinrobotics.com>
2022-02-23 16:10:11 +0000

    Add pcl::PointNormal template instantiation to fast_gicp.cpp

--- src/fast_gicp/gicp/fast_gicp.cpp
+++ src/fast_gicp/gicp/fast_gicp.cpp
@@ -3,3 +3,4 @@
 
 template class fast_gicp::FastGICP<pcl::PointXYZ, pcl::PointXYZ>;
 template class fast_gicp::FastGICP<pcl::PointXYZI, pcl::PointXYZI>;
+template class fast_gicp::FastGICP<pcl::PointNormal, pcl::PointNormal>;

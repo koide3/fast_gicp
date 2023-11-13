83454bd5e6083ae9a0064e1dc9af61a8eced1180
koide3 <31344317+koide3@users.noreply.github.com>
2023-11-13 16:13:52 +0900

    se3 expmap (#139)
    Co-authored-by: k.koide <k.koide@aist.go.jp>

--- include/fast_gicp/gicp/impl/lsq_registration_impl.hpp
+++ include/fast_gicp/gicp/impl/lsq_registration_impl.hpp
@@ -111,9 +111,7 @@ bool LsqRegistration<PointTarget, PointSource>::step_gn(Eigen::Isometry3d& x0, E
   Eigen::LDLT<Eigen::Matrix<double, 6, 6>> solver(H);
   Eigen::Matrix<double, 6, 1> d = solver.solve(-b);
 
-  delta.setIdentity();
-  delta.linear() = so3_exp(d.head<3>()).toRotationMatrix();
-  delta.translation() = d.tail<3>();
+  delta = se3_exp(d);
 
   x0 = delta * x0;
   final_hessian_ = H;
@@ -136,9 +134,7 @@ bool LsqRegistration<PointTarget, PointSource>::step_lm(Eigen::Isometry3d& x0, E
     Eigen::LDLT<Eigen::Matrix<double, 6, 6>> solver(H + lm_lambda_ * Eigen::Matrix<double, 6, 6>::Identity());
     Eigen::Matrix<double, 6, 1> d = solver.solve(-b);
 
-    delta.setIdentity();
-    delta.linear() = so3_exp(d.head<3>()).toRotationMatrix();
-    delta.translation() = d.tail<3>();
+    delta = se3_exp(d);
 
     Eigen::Isometry3d xi = delta * x0;
     double yi = compute_error(xi);
